725b714b34e5748db6ed51f1a33d04264dda9f24
Jahongir Akhmadov <104265452+Akhrasmiy@users.noreply.github.com>
2025-10-27 17:49:00 +0500

    Update index.js

--- db/index.js
+++ db/index.js
@@ -2,10 +2,7 @@ const mongoose = require('mongoose');
 
 module.exports = function () {
   return mongoose
-    .connect('mongodb://127.0.0.1:27017/saver', {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    })
+    .connect('mongodb://127.0.0.1:27017/saver')
     .then(() => {
       console.log('DB ga ulandi.');
     })
